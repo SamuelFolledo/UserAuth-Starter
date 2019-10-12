8ac6395a89daf1aac551644a93664eb5b7da6785
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-12 14:58:31 -0700

    [add] init AuthVC

---
+++ FolledoUserAuth/User Authentication/AuthenticationVC.swift
@@ -0,0 +1,44 @@
+//
+//  AuthenticationVC.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 10/12/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationVC: UIViewController {
+
+//MARK: IBOulets
+        
+        
+        
+        
+        
+//MARK: Properties
+        
+        
+        
+        
+        
+//MARK: LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+//MARK: Methods
+    
+    
+    
+//MARK: Helpers
+    
+    
+    
+//MARK: IBActions
+
+}
