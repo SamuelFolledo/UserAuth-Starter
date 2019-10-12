a6bd773302b8fcdbdbde83b65182bde089250a18
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-12 11:59:44 -0700

    init commit

---
+++ FolledoUserAuth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 10/12/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
