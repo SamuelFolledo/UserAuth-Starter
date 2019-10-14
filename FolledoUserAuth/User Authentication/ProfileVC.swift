cb871f7894d92cdcfd8774339fc1b241d651c6b2
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-14 00:58:29 -0700

    [add] some code for profile and UI updates

--- FolledoUserAuth/User Authentication/ProfileVC.swift
+++ FolledoUserAuth/User Authentication/ProfileVC.swift
@@ -13,8 +13,10 @@ class ProfileVC: UIViewController {
 
 //MARK: IBOulets
        
-    
-    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var firstTextField: UITextField!
+    @IBOutlet weak var lastTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
     
         
 //MARK: Properties
@@ -39,23 +41,34 @@ class ProfileVC: UIViewController {
     func setUp() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissTap(_:)))
         self.view.addGestureRecognizer(tap)
-        
-    
     }
         
         
 
         
 //MARK: IBActions
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        popBack(2)
+    }
     
-    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     
 //MARK: Helpers
     @objc func handleDismissTap(_ gesture: UITapGestureRecognizer) { //dismiss fields
         self.view.endEditing(false)
     }
-        
+    
+    func popBack(_ nb: Int) { //method that pops View controller to a certain amount nb
+        if let viewControllers: [UIViewController] = self.navigationController?.viewControllers {
+            guard viewControllers.count < nb else {
+                self.navigationController?.popToViewController(viewControllers[viewControllers.count - nb], animated: true)
+                return
+            }
+        }
+    }
         
 
 }
