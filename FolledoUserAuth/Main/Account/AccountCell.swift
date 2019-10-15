ba2aa5b49aecc761cf2ddb9331d10484d2fd78b7
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-14 20:40:47 -0700

    [add] environments for Account Controller and Account Cell. [move] some files to its own directory

---
+++ FolledoUserAuth/Main/Account/AccountCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountCell.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 10/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class AccountCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
