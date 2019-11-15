6cdcf59817ac4172c106a968dd7e9b58809c140a
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-14 23:55:19 -0800

    [add] AuthenticationVC now looks really good

--- FolledoUserAuth/User Authentication/AuthenticationVC.swift
+++ FolledoUserAuth/User Authentication/AuthenticationVC.swift
@@ -10,21 +10,65 @@ import UIKit
 
 class AuthenticationVC: UIViewController {
 
+//MARK: IBOulets
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var topTextField: UnderlinedTextField!
+    @IBOutlet weak var topErrorLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var bottomTextField: UnderlinedTextField!
+    @IBOutlet weak var bottomErrorLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    
+//MARK: Properties
+    
+    
+    
+    
+//MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
-    */
-
+    
+    
+    
+    
+//MARK: Methods
+    private func setupViews() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissTap(_:)))
+        self.view.addGestureRecognizer(tap)
+        topErrorLabel.isHidden = true
+        topErrorLabel.textColor = .systemRed
+        bottomErrorLabel.isHidden = true
+        bottomErrorLabel.textColor = .systemRed
+        topLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        bottomLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        continueButton.layer.cornerRadius = continueButton.frame.height / 10
+        continueButton.clipsToBounds = true
+    }
+    
+    
+//MARK: Helpers
+    @objc func handleDismissTap(_ gesture: UITapGestureRecognizer) { //dismiss fields
+        self.view.endEditing(false)
+    }
+    
+    
+//MARK: IBActions
+    @IBAction func continueButtonTapped(_ sender: Any) {
+        self.performSegue(withIdentifier: kTONAMEVC, sender: nil)
+    }
+    
+    @IBAction func backButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
