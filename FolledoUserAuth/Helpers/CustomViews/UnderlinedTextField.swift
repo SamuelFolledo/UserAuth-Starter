4ba89c3e05ae611d0745d6e2002050c1252009ae
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-14 22:05:51 -0800

    [add] a custom textfield class with underline which only has the bottom line

---
+++ FolledoUserAuth/Helpers/CustomViews/UnderlinedTextField.swift
@@ -0,0 +1,36 @@
+//
+//  UnderlinedTextField.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 11/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class UnderlinedTextField: UITextField {
+    private let defaultUnderlineColor = UIColor.black
+    private let bottomLine = UIView()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        borderStyle = .none
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        bottomLine.backgroundColor = defaultUnderlineColor
+
+        self.addSubview(bottomLine)
+        bottomLine.topAnchor.constraint(equalTo: self.bottomAnchor, constant: 1).isActive = true
+        bottomLine.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        bottomLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+
+    public func setUnderlineColor(color: UIColor = .red) {
+        bottomLine.backgroundColor = color
+    }
+
+    public func setDefaultUnderlineColor() {
+        bottomLine.backgroundColor = defaultUnderlineColor
+    }
+}
