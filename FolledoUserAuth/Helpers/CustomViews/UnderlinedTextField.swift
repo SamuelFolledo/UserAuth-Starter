1d8f101f0e3bce315ac1d59158e4ed2f118e7152
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-14 23:46:50 -0800

    [add] a way to make sure the UnderLinedTextField looks good

--- FolledoUserAuth/Helpers/CustomViews/UnderlinedTextField.swift
+++ FolledoUserAuth/Helpers/CustomViews/UnderlinedTextField.swift
@@ -5,6 +5,7 @@
 //  Created by Macbook Pro 15 on 11/14/19.
 //  Copyright © 2019 SamuelFolledo. All rights reserved.
 //
+//  inspired by Stackoverflow's post https://stackoverflow.com/questions/31107994/how-to-only-show-bottom-border-of-uitextfield-in-swift/31108018
 
 import UIKit
 
@@ -15,10 +16,13 @@ class UnderlinedTextField: UITextField {
     override func awakeFromNib() {
         super.awakeFromNib()
         borderStyle = .none
+        contentVerticalAlignment = .center //added
+        clearButtonMode = .unlessEditing //added
+        font = UIFont.boldSystemFont(ofSize: 18) //added
         bottomLine.translatesAutoresizingMaskIntoConstraints = false
         bottomLine.backgroundColor = defaultUnderlineColor
         self.addSubview(bottomLine)
-        bottomLine.topAnchor.constraint(equalTo: self.bottomAnchor, constant: 1).isActive = true
+        bottomLine.topAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true //updated to make the line closer to the text
         bottomLine.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
         bottomLine.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
         bottomLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
---
+++ FolledoUserAuth/User Authentication/AuthenticationVC.swift
@@ -0,0 +1,30 @@
+//
+//  AuthenticationVC.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 11/14/19.
+//  Copyright © 2019 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class AuthenticationVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
