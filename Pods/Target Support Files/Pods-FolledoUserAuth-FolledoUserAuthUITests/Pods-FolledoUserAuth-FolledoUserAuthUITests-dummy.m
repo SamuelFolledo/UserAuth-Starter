05a7b775620f0b0a9fcdc963876f3d24c7229f0e
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-15 19:14:48 -0800

    [removed] pods/

--- Pods/Target Support Files/Pods-FolledoUserAuth-FolledoUserAuthUITests/Pods-FolledoUserAuth-FolledoUserAuthUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FolledoUserAuth_FolledoUserAuthUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FolledoUserAuth_FolledoUserAuthUITests
-@end
--- Pods/Target Support Files/Pods-FolledoUserAuth/Pods-FolledoUserAuth-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FolledoUserAuth : NSObject
-@end
-@implementation PodsDummy_Pods_FolledoUserAuth
-@end
