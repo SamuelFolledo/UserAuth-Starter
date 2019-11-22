1a7ac8e9a37879dd3e6c9a441aa0f5ff844ac484
Samuel Folledo <samuelfolledo@gmail.com>
2019-11-21 21:04:12 -0800

    [add] View Model that has the text as well as setup functions for the views in AuthVC

--- FolledoUserAuth/ViewModel/UserAuthenticationVM.swift
+++ FolledoUserAuth/ViewModel/UserAuthenticationVM.swift
@@ -11,7 +11,65 @@ import UIKit
 public final class UserAuthenticationViewModel {
     public let isEmailAuthentication: Bool
     
+    public let navigationTitle: String
+    public let topLabelText: String
+    public var topErrorLabelText: String
+    public let bottomLabelText: String
+    public var bottomErrorLabelText: String
+    public var continueButtonTitle: String
+    
     public init(isEmailAuthentication: Bool) {
         self.isEmailAuthentication = isEmailAuthentication
+        topErrorLabelText = ""
+        bottomErrorLabelText = ""
+        if isEmailAuthentication {
+            navigationTitle = "Email Authentication"
+            topLabelText = "Email"
+            bottomLabelText = "Password"
+            continueButtonTitle = "Create Account/Login"
+        } else {
+            navigationTitle = "Phone Authentication"
+            topLabelText = "Phone Number"
+            bottomLabelText = "Code"
+            continueButtonTitle = "Text Password"
+        }
+    }
+    
+    public func setupTopLabel(label: UILabel) {
+        label.isAuthLabel()
+        label.text = isEmailAuthentication ? "Email" : "Phone Number"
+    }
+    
+    public func setupBottomLabel(label: UILabel) {
+        label.isAuthLabel()
+        if isEmailAuthentication {
+            label.text = "Password"
+            label.isHidden = false
+        } else {
+            label.text = "Code"
+            label.isHidden = true
+        }
+    }
+    
+    public func setupErrorLabel(label: UILabel, isHidden: Bool = true) {
+        label.isAuthErrorLabel()
+        label.text = ""
+        label.textColor = .red
+        label.isHidden = isHidden
+    }
+    
+    func setupTextFields(top topTF: UnderlinedTextField, bottom bottomTF: UnderlinedTextField, hideBottom hasPhoneCode: Bool = false) {
+        if isEmailAuthentication {
+            topTF.isEmailTextField()
+            bottomTF.isPasswordTextField()
+        } else {
+            topTF.isPhoneTextField()
+            bottomTF.isPhoneCodeTextField(isHidden: !hasPhoneCode) //if hasPhoneCode, then hide == opposite of hasPhoneCode
+        }
+    }
+    
+    func setupContinueButton(button: UIButton) {
+        button.isAuthButton()
+        button.setTitle(self.continueButtonTitle, for: .normal)
     }
 }
