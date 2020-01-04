94ebb7b8d6034f0563f625c32e0ad1f9b8573a77
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-04 14:16:38 -0800

    [add] updated access rights from let to public [add] verification variable

--- FolledoUserAuth/Helpers/Constants.swift
+++ FolledoUserAuth/Helpers/Constants.swift
@@ -10,108 +10,109 @@ import Foundation
 import UIKit
 import FirebaseDatabase
 
-let firDatabase = Database.database().reference()
+public let firDatabase = Database.database().reference()
+public let kVERIFICATIONCODE = "firebase_verification" //for phone auth
 
 //ids and keys for one signal
 public let kONESIGNALAPPID: String = "586d3ef3-6411-41d0-ab81-2a797a16a50b"
 public let kONESIGNALID: String = "OneSignalId"
-let kUSERID: String = "userID"
-let kUSERNAME: String = "username"
-let kFIRSTNAME: String = "firstName"
-let kLASTNAME: String = "lastName"
-let kFULLNAME: String = "fullName"
-let kEMAIL: String = "email"
-let kAVATARURL: String = "avatarURL"
-let kCURRENTUSER: String = "currentUser" //for userDefaults
-let kUSERS: String = "user" //for firebase
-let kMESSAGES: String = "message"
-let kPUSHID: String = "pushId"
+public let kUSERID: String = "userID"
+public let kUSERNAME: String = "username"
+public let kFIRSTNAME: String = "firstName"
+public let kLASTNAME: String = "lastName"
+public let kFULLNAME: String = "fullName"
+public let kEMAIL: String = "email"
+public let kAVATARURL: String = "avatarURL"
+public let kCURRENTUSER: String = "currentUser" //for userDefaults
+public let kUSERS: String = "user" //for firebase
+public let kMESSAGES: String = "message"
+public let kPUSHID: String = "pushId"
 
 //MARK: Other Constants
-let kCREATEDAT: String = "createdAt"
-let kUPDATEDAT: String = "updatedAt"
-let kGAMESESSIONS: String = "gameSessions"
-let kUSERTOGAMESESSIONS: String = "user-gameSessions"
-let kPLAYER1ID: String = "player1Id"
-let kPLAYER2ID: String = "player2Id"
-let kPLAYER1NAME: String = "player1Name"
-let kPLAYER2NAME: String = "player2Name"
-let kPLAYER1EMAIL: String = "player1Email"
-let kPLAYER2EMAIL: String = "player2Email"
-let kPLAYER1AVATARURL: String = "player1AvatarUrl"
-let kPLAYER2AVATARURL: String = "player2AvatarUrl"
-let kPLAYER1IMAGE: String = "player1Image"
-let kPLAYER2IMAGE: String = "player2Image"
-let kPLAYER1HP: String = "player1Hp"
-let kPLAYER2HP: String = "player2Hp"
-let kP1MOVE: String = "p1Move"
-let kP2MOVE: String = "p2Move"
+public let kCREATEDAT: String = "createdAt"
+public let kUPDATEDAT: String = "updatedAt"
+public let kGAMESESSIONS: String = "gameSessions"
+public let kUSERTOGAMESESSIONS: String = "user-gameSessions"
+public let kPLAYER1ID: String = "player1Id"
+public let kPLAYER2ID: String = "player2Id"
+public let kPLAYER1NAME: String = "player1Name"
+public let kPLAYER2NAME: String = "player2Name"
+public let kPLAYER1EMAIL: String = "player1Email"
+public let kPLAYER2EMAIL: String = "player2Email"
+public let kPLAYER1AVATARURL: String = "player1AvatarUrl"
+public let kPLAYER2AVATARURL: String = "player2AvatarUrl"
+public let kPLAYER1IMAGE: String = "player1Image"
+public let kPLAYER2IMAGE: String = "player2Image"
+public let kPLAYER1HP: String = "player1Hp"
+public let kPLAYER2HP: String = "player2Hp"
+public let kP1MOVE: String = "p1Move"
+public let kP2MOVE: String = "p2Move"
 
-let kWINNERUID: String = "winnerUid"
-let kCURRENTGAME: String = "currentGame"
-let kGAMEATTACK: String = "gameAttack"
-let kGAMEMOVE: String = "gameMove"
-let kMOVES: String = "moves"
-let kTURNCOUNT: String = "turnCount"
-let kROUNDNUMBER: String = "roundNumber"
-let kROUNDS: String = "rounds"
-let kGAMEID: String = "gameId"
-let kTIMESTAMP: String = "timeStamp"
-let kGAMEHISTORY: String = "gameHistory"
-//let kTURNCOUNTER: String = "turnCounter"
+public let kWINNERUID: String = "winnerUid"
+public let kCURRENTGAME: String = "currentGame"
+public let kGAMEATTACK: String = "gameAttack"
+public let kGAMEMOVE: String = "gameMove"
+public let kMOVES: String = "moves"
+public let kTURNCOUNT: String = "turnCount"
+public let kROUNDNUMBER: String = "roundNumber"
+public let kROUNDS: String = "rounds"
+public let kGAMEID: String = "gameId"
+public let kTIMESTAMP: String = "timeStamp"
+public let kGAMEHISTORY: String = "gameHistory"
+//public let kTURNCOUNTER: String = "turnCounter"
 
-let kOPPONENTNAME: String = "opponentName"
-let kOPPONENTAVATARTURL : String = "opponentAvatarUrl"
-let kHPLEFT: String = "hpLeft"
+public let kOPPONENTNAME: String = "opponentName"
+public let kOPPONENTAVATARTURL : String = "opponentAvatarUrl"
+public let kHPLEFT: String = "hpLeft"
 
 //storyboard VC Identifiers
-let kHOMEVIEWCONTROLLER: String = "homeVC"
-let kPREGAMEVIEWCONTROLLER: String = "preGameVC"
-let kGAMEVIEWCONTROLLER: String = "gameVC"
-let kGAMEOVERVIEWCONTROLLER: String = "gameOverVC"
-let kGAMEHISTORYVIEWCONTROLLER: String = "gameHistoryVC"
-let kGAMEHISTORYCELL: String = "gameHistoryCell"
+public let kHOMEVIEWCONTROLLER: String = "homeVC"
+public let kPREGAMEVIEWCONTROLLER: String = "preGameVC"
+public let kGAMEVIEWCONTROLLER: String = "gameVC"
+public let kGAMEOVERVIEWCONTROLLER: String = "gameOverVC"
+public let kGAMEHISTORYVIEWCONTROLLER: String = "gameHistoryVC"
+public let kGAMEHISTORYCELL: String = "gameHistoryCell"
 
 //User info
-let kGAMESTATS: String = "gameStats" //this is for Firebase/users/uid/gameStats
-let kWINLOSESTAT: String = "winLoseStat"
-let kWINS: String = "wins"
-let kLOSES: String = "loses"
-let kMATCHESUID: String = "matchesUid"
-let kMATCHESDICTIONARY: String = "matchesDictionary"
-let kEXPERIENCES: String = "experiences"
-let kMAXEXPERIENCE: String = "maxExperience"
-let kLEVEL: String = "level"
-let kRESULT: String = "result"
-let kOPPONENTUID: String = "opponentUid"
+public let kGAMESTATS: String = "gameStats" //this is for Firebase/users/uid/gameStats
+public let kWINLOSESTAT: String = "winLoseStat"
+public let kWINS: String = "wins"
+public let kLOSES: String = "loses"
+public let kMATCHESUID: String = "matchesUid"
+public let kMATCHESDICTIONARY: String = "matchesDictionary"
+public let kEXPERIENCES: String = "experiences"
+public let kMAXEXPERIENCE: String = "maxExperience"
+public let kLEVEL: String = "level"
+public let kRESULT: String = "result"
+public let kOPPONENTUID: String = "opponentUid"
 
 //colors from UIColor extension
-let kCOLOR_FFFFFF: UIColor = UIColor(rgb: 0xFFFFFF)
-let kCOLOR_1B1E1F: UIColor = UIColor(rgb: 0x1B1E1F)
-let kCOLOR_5F6063: UIColor = UIColor(rgb: 0x5F6063)
-let kCOLOR_0E5C89: UIColor = UIColor(rgb: 0x0E5C89)
-let kCOLOR_F9F9F9: UIColor = UIColor(rgb: 0xF9F9F9)
+public let kCOLOR_FFFFFF: UIColor = UIColor(rgb: 0xFFFFFF)
+public let kCOLOR_1B1E1F: UIColor = UIColor(rgb: 0x1B1E1F)
+public let kCOLOR_5F6063: UIColor = UIColor(rgb: 0x5F6063)
+public let kCOLOR_0E5C89: UIColor = UIColor(rgb: 0x0E5C89)
+public let kCOLOR_F9F9F9: UIColor = UIColor(rgb: 0xF9F9F9)
 
-let kREDCGCOLOR = UIColor.red.cgColor
-let kCLEARCGCOLOR = UIColor.clear.cgColor
-let kGREENCGCOLOR = UIColor.green.cgColor
+public let kREDCGCOLOR = UIColor.red.cgColor
+public let kCLEARCGCOLOR = UIColor.clear.cgColor
+public let kGREENCGCOLOR = UIColor.green.cgColor
 
 //fonts
-let kHEADERTEXT: UIFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
+public let kHEADERTEXT: UIFont = UIFont.systemFont(ofSize: 17, weight: .semibold)
 
 //controller storyboard id
-let kCHATCONTROLLER: String = "chatController"
-let kLOGINCONTROLLER: String = "loginController"
-let kANIMATIONCONTROLLER: String = "animationController"
-let kMENUCONTROLLER: String = "menuController"
+public let kCHATCONTROLLER: String = "chatController"
+public let kLOGINCONTROLLER: String = "loginController"
+public let kANIMATIONCONTROLLER: String = "animationController"
+public let kMENUCONTROLLER: String = "menuController"
 
-let kREGISTEREDUSERS: String = "registeredUsers"
-let kPHONENUMBER: String = "phoneNumber"
+public let kREGISTEREDUSERS: String = "registeredUsers"
+public let kPHONENUMBER: String = "phoneNumber"
 
-let kFINISHREGISTRATIONVC: String = "finishRegistrationVC"
-let kAUTHENTICATIONVC: String = "authenticationVC"
+public let kFINISHREGISTRATIONVC: String = "finishRegistrationVC"
+public let kAUTHENTICATIONVC: String = "authenticationVC"
 
 //Storyboard Identifiers
-let kTOAUTHENTICATIONVC: String = "toAuthenticationVC"
-let kTONAMEVC: String = "toNameVC"
-let kTOAUTHMENUVC: String = "toAuthMenuVC"
+public let kTOAUTHENTICATIONVC: String = "toAuthenticationVC"
+public let kTONAMEVC: String = "toNameVC"
+public let kTOAUTHMENUVC: String = "toAuthMenuVC"
