bbf4dcb86bc07cd2599f028664d3023c4141eccc
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-05 01:43:17 -0800

    [add] Code that is needed in order to make texting code works

--- FolledoUserAuth/SceneDelegate.swift
+++ FolledoUserAuth/SceneDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -18,6 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) { //required if you have Scene Delegate
+      for urlContext in URLContexts {
+          let url = urlContext.url
+          Auth.auth().canHandle(url)
+      }
+      // URL not auth related, developer should handle it.
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
