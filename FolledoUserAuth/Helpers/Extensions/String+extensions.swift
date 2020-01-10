cadda0ea7c4f1a7a172642a411b64104afd5b764
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-09 23:19:28 -0800

    [add] comments on isValidEmail regex

--- FolledoUserAuth/Helpers/Extensions/String+extensions.swift
+++ FolledoUserAuth/Helpers/Extensions/String+extensions.swift
@@ -45,7 +45,7 @@ extension String {
     }
     
     var isValidEmail: Bool {
-        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}" //\\. is escape character for dot
         let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: self)
     }
