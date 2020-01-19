9fdd687b00f7ea669071927ac3d2f6368f965b9f
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-18 16:18:00 -0800

    [add] updated from swift 4 to download image from Storage

--- FolledoUserAuth/Helpers/Extensions/UIImageView+extensions.swift
+++ FolledoUserAuth/Helpers/Extensions/UIImageView+extensions.swift
@@ -10,12 +10,12 @@ import Foundation
 import UIKit
 
 extension UIImageView {
-    func downloaded(fromURL url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+    func downloaded(fromURL url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) { //method that will download its own image from a url
         contentMode = mode
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                //let mimeType = response?.mimeType, mimeType.hasPrefix("image"), //error here
                 let data = data, error == nil,
                 let image = UIImage(data: data)
                 else { return }
@@ -24,7 +24,7 @@ extension UIImageView {
                 }
             }.resume()
     }
-    func downloaded(fromLink link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+    func downloaded(fromLink link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) { //method that will download its own image from a url
         guard let url = URL(string: link) else { return }
         downloaded(fromURL: url, contentMode: mode)
     }
