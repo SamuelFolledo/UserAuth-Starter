02fbf99b8c6f3150c36295e21242e42954d6aadc
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-18 19:38:35 -0800

    [add] improve register/signin with phone and also no checks if it's first time or not to reduce fetching

--- FolledoUserAuth/Model/User.swift
+++ FolledoUserAuth/Model/User.swift
@@ -301,28 +301,47 @@ func assignFullName(fName: String, lName: String) -> String {
 
 //MARK: Phone Authentication
 extension User {
-    class func registerUserWith(phoneNumber: String, verificationCode: String, completion: @escaping (_ error: Error?, _ shouldLogin: Bool) -> Void) {
+    class func registerUserWith(phoneNumber: String, verificationCode: String, completion: @escaping (_ error: String?, _ shouldLogin: Bool) -> Void) {
         let verificationID = UserDefaults.standard.value(forKey: kVERIFICATIONCODE) //kVERIFICATIONCODE = "firebase_verification" //Once our user inputs phone number and request a code, firebase will send the modification code which is not the password code. This code is sent by Firebase in the background to identify if the application is actually running on the device that is requesting the code.
         let credentials = PhoneAuthProvider.provider().credential(withVerificationID: verificationID as! String, verificationCode: verificationCode)
         print("Phone = \(phoneNumber) == \(verificationCode)")
         Auth.auth().signIn(with: credentials) { (userResult, error) in //Asynchronously signs in to Firebase with the given 3rd-party credentials (e.g. a Facebook login Access Token, a Google ID Token/Access Token pair, etc.) and returns additional identity provider data.
             if let error = error { //if there's error put false on completion's shouldLogin parameter
-                completion(error, false)
+                completion(error.localizedDescription, false)
             }
-            fetchUserWith(userId: (userResult?.user.uid)!, completion: { (user) in //check if there is user then logged in else register
-                if user != nil && user?.firstName != "" { //if user is nil and user has a first name, provides extra protection
-                    saveUserLocally(user: user!) //save user in our UserDefaults. We dont need to save in background because we are already getting/fetching the user
-                    completion(error, true) //call our callback function to exit and finally input the error or shouldLogin to true
-                } else { //register the user
-                    guard let uid: String = userResult?.user.uid else { return }
-                    guard let phoneNumber = userResult?.user.phoneNumber else { return }
-                    let user = User(_userId: uid, _username: "", _firstName: "", _lastName: "", _email: "", _phoneNumber: phoneNumber, _imageUrl: "", _authTypes: [.phone], _createdAt: Date(), _updatedAt: Date())
-//                    let user = User(_userID: uid, _phoneNumber: phoneNumber)
-                    saveUserLocally(user: user) //now we have the newly registered user, save it locally and in background
-                    saveUserInBackground(user: user)
-                    completion(error, false) //shouldLogin = false because we need to finish registering the user
+            guard let userResult = userResult else { return } //userResult contains lots of important info we will need in the future
+            //            print("USER RESULT = \(userResult)\nUSER ADDITIONAL INFO = \(userResult.additionalUserInfo)\n\n \(userResult.credential)")
+            let user: User = User(_userId: userResult.user.uid, _username: "", _firstName: "", _lastName: "", _email: "", _phoneNumber: userResult.user.phoneNumber!, _imageUrl: "", _authTypes: [.phone], _createdAt: Date(), _updatedAt: Date())
+            if userResult.additionalUserInfo!.isNewUser { //if new user, save locally and finish registering
+                saveUserLocally(user: user) //now we have the newly registered user, save it locally and in background
+                saveUserInBackground(user: user)
+                completion(nil, false) //shouldLogin = false because we need to finish registering the user
+            } else { //login
+                fetchUserWith(userId: user.userId) { (user) in
+                    saveUserLocally(user: user!) //we dont need to save in background because we are already getting/fetching the user
+                    if user != nil && user?.firstName != "" { //if user is nil and user has a first name, provides extra protection
+                        completion(nil, false) //this will rarely get executed, but just in case we have a user who for some reason is not first time but has no first name
+                    } else { //user has previous finished registering, so we can log them in
+                        completion(nil, true)
+                    }
                 }
-            })
+            }
+            
+            
+            //            fetchUserWith(userId: (userResult.user.uid), completion: { (user) in //check if there is user then logged in else register
+            //                if user != nil && user?.firstName != "" { //if user is nil and user has a first name, provides extra protection
+            //                    saveUserLocally(user: user!) //save user in our UserDefaults. We dont need to save in background because we are already getting/fetching the user
+            //                    completion(error, true) //call our callback function to exit and finally input the error or shouldLogin to true
+            //                } else { //register the user
+            //                    guard let uid: String = userResult?.user.uid else { return }
+            //                    guard let phoneNumber = userResult?.user.phoneNumber else { return }
+            //                    let user = User(_userId: uid, _username: "", _firstName: "", _lastName: "", _email: "", _phoneNumber: phoneNumber, _imageUrl: "", _authTypes: [.phone], _createdAt: Date(), _updatedAt: Date())
+            ////                    let user = User(_userID: uid, _phoneNumber: phoneNumber)
+            //                    saveUserLocally(user: user) //now we have the newly registered user, save it locally and in background
+            //                    saveUserInBackground(user: user)
+            //                    completion(error, false) //shouldLogin = false because we need to finish registering the user
+            //                }
+            //            })
         }
     }
 }
