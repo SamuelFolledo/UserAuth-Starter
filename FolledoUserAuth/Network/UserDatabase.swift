bb36ac5f2f2320a5971f57b534bcc627c441ffeb
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-19 15:18:55 -0800

    [add] UserDatabase and UserStorage separation of files

---
+++ FolledoUserAuth/Network/UserDatabase.swift
@@ -0,0 +1,9 @@
+//
+//  UserDatabase.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 1/19/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
---
+++ FolledoUserAuth/Network/UserStorage.swift
@@ -0,0 +1,9 @@
+//
+//  UserStorage.swift
+//  FolledoUserAuth
+//
+//  Created by Macbook Pro 15 on 1/19/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
