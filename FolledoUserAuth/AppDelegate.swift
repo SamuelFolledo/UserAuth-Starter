eaa7bec2bc958ed4338885031a276a3125501f51
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-19 16:41:40 -0800

    [add] needed methods and import for Facebook Auth

--- FolledoUserAuth/AppDelegate.swift
+++ FolledoUserAuth/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Firebase
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions) //1. Facebook needed for Facebook Auth
         return true
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool { //2. Facebook needed for Facebook Auth. Now we can create our buttons
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
