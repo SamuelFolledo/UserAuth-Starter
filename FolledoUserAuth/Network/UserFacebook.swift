3b0c0ea80e04a32ef1dcb4e69e7ee67a235a1f4a
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-19 19:47:59 -0800

    [add] getFacebookProfilePic method which takes it from userDetails gotten

--- FolledoUserAuth/Network/UserFacebook.swift
+++ FolledoUserAuth/Network/UserFacebook.swift
@@ -7,3 +7,21 @@
 //
 
 import UIKit
+
+//get user's Profile Picture
+func getFacebookProfilePic(userDetails: [String: AnyObject], completion: @escaping (_ image: UIImage?, _ error: String?) -> Void) { //from user details, get the url which contains the image, and return the image
+    guard let profilePictureObj: [String: AnyObject] = userDetails["picture"] as? [String: AnyObject] else { completion(nil, "Can't get picture"); return }
+    guard let data: [String: AnyObject] = profilePictureObj["data"] as? [String: AnyObject] else { completion(nil, "Can't get profile picture's data"); return }
+    guard let profilePicUrlString = data["url"] as? String else { completion(nil, "Can't get profile picture's url"); return }
+//    guard let profilePicUrlString = data["url"]?.absoluteString else { completion(nil, "Can't get profile picture's url"); return }
+    guard let profilePicUrl = URL(string: profilePicUrlString) else { completion(nil, "Can't get picture"); return }
+    do { //catch any errors
+        let imageData = try Data(contentsOf: profilePicUrl) //create imageData from the pic's url
+        DispatchQueue.main.async {
+            let userImage = UIImage(data: imageData) //turn imageData to a UIImage
+            completion(userImage, nil)
+        }
+    } catch let error {
+        completion(nil, error.localizedDescription)
+    }
+}
