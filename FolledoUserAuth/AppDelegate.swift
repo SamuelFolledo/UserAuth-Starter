eb258f8fcb0130109594cf023be9815aaabdf59c
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-20 20:56:13 -0800

    [add] necessary methods for Google signin

--- FolledoUserAuth/AppDelegate.swift
+++ FolledoUserAuth/AppDelegate.swift
@@ -9,19 +9,55 @@
 import UIKit
 import Firebase
 import FacebookCore
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions) //1. Facebook needed for Facebook Auth
+        ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions) //1/2 Facebook needed for Facebook Auth
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID //1/5 Google Signin
+        GIDSignIn.sharedInstance().delegate = self //2/5 Google Signin
         return true
     }
 
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool { //2. Facebook needed for Facebook Auth. Now we can create our buttons
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool { //2/2 Facebook needed for Facebook Auth. Now we can create our buttons
         return ApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool { //3/5 Google Signin
+        return GIDSignIn.sharedInstance().handle(url)
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) { //4/5 Google Signin
+        // ...
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("AppDelegate Error with Google Signin \(error.localizedDescription)")
+            }
+            return
+        }
+        // Perform any operations on signed in user here.
+        let userId = user.userID                  // For client-side use only!
+        let idToken = user.authentication.idToken // Safe to send to the server
+        let fullName = user.profile.name
+        let givenName = user.profile.givenName
+        let familyName = user.profile.familyName
+        let email = user.profile.email
+        print("User we have from App Delegate's Google Signin = \(String(describing: fullName))")
+        guard let authentication = user.authentication else { return }
+        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+        print("AppDelegate got Google Sigin credential \(credential)")
+        // ...
+    }
+
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) { //5/5 Google Signin
+        // Perform any operations when the user disconnects from app here.
+        // ...
     }
     
     // MARK: UISceneSession Lifecycle
