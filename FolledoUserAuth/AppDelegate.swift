a120f19468a19c26a4d8aa0487f5b6eca587c289
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-20 22:34:36 -0800

    [add] remove some Google signin unnecessary implementations here

--- FolledoUserAuth/AppDelegate.swift
+++ FolledoUserAuth/AppDelegate.swift
@@ -12,14 +12,14 @@ import FacebookCore
 import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions) //1/2 Facebook needed for Facebook Auth
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID //1/5 Google Signin
-        GIDSignIn.sharedInstance().delegate = self //2/5 Google Signin
+//        GIDSignIn.sharedInstance().delegate = self //2/5 Google Signin
         return true
     }
 
@@ -31,29 +31,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
         return GIDSignIn.sharedInstance().handle(url)
     }
     
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) { //4/5 Google Signin
-        // ...
-        if let error = error {
-            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
-                print("The user has not signed in before or they have since signed out.")
-            } else {
-                print("AppDelegate Error with Google Signin \(error.localizedDescription)")
-            }
-            return
-        }
-        // Perform any operations on signed in user here.
-        let userId = user.userID                  // For client-side use only!
-        let idToken = user.authentication.idToken // Safe to send to the server
-        let fullName = user.profile.name
-        let givenName = user.profile.givenName
-        let familyName = user.profile.familyName
-        let email = user.profile.email
-        print("User we have from App Delegate's Google Signin = \(String(describing: fullName))")
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
-        print("AppDelegate got Google Sigin credential \(credential)")
-        // ...
-    }
+//    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+//        // ...
+//        if let error = error {
+//            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+//                print("The user has not signed in before or they have since signed out.")
+//            } else {
+//                print("AppDelegate Error with Google Signin \(error.localizedDescription)")
+//            }
+//            return
+//        }
+//        // Perform any operations on signed in user here.
+//        let userId = user.userID // For client-side use only!
+//        let idToken = user.authentication.idToken // Safe to send to the server
+//        let fullName = user.profile.name
+//        let givenName = user.profile.givenName
+//        let familyName = user.profile.familyName
+//        let email = user.profile.email
+//        print("User we have from App Delegate's Google Signin = \(String(describing: fullName))")
+//        guard let authentication = user.authentication else { return }
+//        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
+//        print("AppDelegate got Google Sigin credential \(credential)")
+//        // ...
+//    }
 
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) { //5/5 Google Signin
         // Perform any operations when the user disconnects from app here.
